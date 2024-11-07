4b8e8b406d75e4d5f96e3d0b86fd56c7460fc94c
Walassseee <walasse.tomaz@gmail.com>
2024-11-07 20:19:03 -0300

    Draft forest descritive analysis post v7

--- posts/forest_descritive/indexsup.R
+++ posts/forest_descritive/indexsup.R
@@ -7,7 +7,6 @@ library(tidyr)
 library(ggplot2)
 library(gridExtra)
 library(scales)
-library(tibble)
 library(gt)
 
 # ETL ##########################################################################
@@ -50,50 +49,6 @@ pevs_valor <- processar_pevs(143)
 
 # EDA ##########################################################################
 
-#| echo: false
-
-library(sidrar)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(scales)
-
-processar_pevs <- function(variavel) {
-  
-  pevs <- get_sidra(x = 291,
-                    geo = "Region",
-                    variable = variavel,
-                    period = c("last" = 24),
-                    classific = "C194",
-                    category = list(c(3455, 3456, 3457, 3460))) |>
-    select(`Grande Região`, Ano, Valor, `Tipo de produto da silvicultura`) |>
-    
-    mutate(`Tipo de produto da silvicultura` = case_when(
-      `Tipo de produto da silvicultura` == "1.1 - Carvão vegetal" ~ "Carvão vegetal",
-      `Tipo de produto da silvicultura` == "1.2 - Lenha" ~ "Lenha",
-      `Tipo de produto da silvicultura` == "1.3 - Madeira em tora" ~ "Madeira em tora",
-      `Tipo de produto da silvicultura` == "2 - Outros produtos" ~ "Outros produtos",
-      TRUE ~ `Tipo de produto da silvicultura`
-    )) |>
-    
-    pivot_wider(
-      names_from = `Tipo de produto da silvicultura`,
-      values_from = Valor,
-      values_fill = 0
-    ) |>
-    
-    mutate(across(c(`Carvão vegetal`, `Lenha`, `Madeira em tora`, `Outros produtos`), 
-                  ~ replace_na(as.numeric(.x), 0))) |>
-    
-    mutate(across(c(`Carvão vegetal`, `Lenha`, `Madeira em tora`, `Outros produtos`), as.numeric)) |>
-    
-    select(`Grande Região`, Ano, `Carvão vegetal`, `Lenha`, `Madeira em tora`, `Outros produtos`)
-  
-  return(pevs)
-}
-
-pevs_valor <- processar_pevs(143)
-
 pevs_filtrado <- pevs_valor |>
   select(-`Grande Região`) |>
   filter(Ano %in% c(2022, 2023)) |>
@@ -125,4 +80,43 @@ pevs_chart <- ggplot(pevs_filtrado, aes(x = Produto, y = Total, fill = as.factor
 
 pevs_chart
 
+pevs_filtrado <- pevs_valor |>
+  select(-`Grande Região`) |>
+  mutate(Ano = as.numeric(Ano)) |>
+  group_by(Ano) |>
+  summarise(across(c(`Carvão vegetal`, `Lenha`, `Madeira em tora`, `Outros produtos`), sum, na.rm = TRUE)) |>
+  pivot_longer(cols = c(`Carvão vegetal`, `Lenha`, `Madeira em tora`, `Outros produtos`), names_to = "Produto", values_to = "Valor Total")
+
+pevs_flow <- ggplot(pevs_filtrado, aes(x = Ano, y = `Valor Total`, color = Produto, group = Produto)) +
+  geom_line(size = 1.1) +
+  geom_point(shape = 21, fill = "white", size = 2.5) +
+  geom_hline(yintercept = 0, color = "#000000", linewidth = 1) +
+  scale_y_continuous(labels = scales::label_number(scale = 1e-6, prefix = "R$", suffix = " Bi")) +
+  scale_x_continuous(breaks = c(2000, 2005, 2010, 2015, 2020, 2023)) +
+  scale_color_manual(values = c("Carvão vegetal" = "#fdbf11", "Lenha" = "#1696d2", "Madeira em tora" = "#55b748", "Outros produtos" = "#000000")) +
+  labs(
+    title = "Produção Florestal nos Últimos Anos",
+    subtitle = "Valor de produção entre 2000 e 2024, R$ Bilhões",
+    x = NULL,
+    y = NULL
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid.major.y = element_line(color = "grey80"),
+    panel.grid.major.x = element_blank(),
+    legend.position = "top",
+    legend.title = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.key.size = unit(0.7, "lines"),
+    axis.text.x = element_text(face = "bold"),
+    plot.title = element_text(hjust = 0, face = "bold"),
+    plot.subtitle = element_text(hjust = 0),
+    plot.caption = element_text(hjust = 0),
+    plot.caption.position = "plot",
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank()
+  )
+
+
+pevs_flow
 
